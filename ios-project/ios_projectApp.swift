a517948dc8908b316769357c8928305c7ab03fd8
thomas <thomas.lissens@student.hogent.be>
2024-01-01 21:41:01 +0100

    Initial Commit

---
+++ ios-project/ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_projectApp.swift
+//  ios-project
+//
+//  Created by Thomas Lissens on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
