aeef85c5f17727763ed511ca624f7e51719834e4
thomas <thomas.lissens@student.hogent.be>
2024-01-07 02:54:35 +0100

    Added game logic + fixed small bugs

--- ios-project/ApplicationView.swift
+++ ios-project/ApplicationView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var routerManager = NavigationRouter()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $routerManager.routes) {
+            NavigationBar()
         }
-        .padding()
+
     }
 }
 
